6d131503b2aee7b8c9dddc519421b380292dcc8b
gumboguru01 <ngumboguru@byupathway.edu>
2025-03-08 21:26:36 +0200

    new code

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int attempts = 0;
+
+            Console.WriteLine("Guess My Number Game!");
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} attempts!");
+                }
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
