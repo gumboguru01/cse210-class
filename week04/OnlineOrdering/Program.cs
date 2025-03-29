be02d0aa3cc470d968471c2c3f47547f0b5ff4f0
gumboguru01 <ngumboguru@byupathway.edu>
2025-03-29 15:48:37 +0200

    initial commit

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,155 @@
 using System;
+using System.Collections.Generic;
+
+class Product
+{
+    private string name;
+    private string productId;
+    private double price;
+    private int quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return price * quantity;
+    }
+
+    public string GetPackingLabel()
+    {
+        return $"{name} (ID: {productId})";
+    }
+}
+
+class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return country.ToLower() == "usa";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{street}\n{city}, {state}\n{country}";
+    }
+}
+
+class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public bool LivesInUSA()
+    {
+        return address.IsInUSA();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{name}\n{address.GetFullAddress()}";
+    }
+}
+
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        this.products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double GetTotalPrice()
+    {
+        double total = 0;
+        foreach (Product product in products)
+        {
+            total += product.GetTotalCost();
+        }
+        total += customer.LivesInUSA() ? 5 : 35;
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing List:\n";
+        foreach (Product product in products)
+        {
+            label += product.GetPackingLabel() + "\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return "Shipping Label:\n" + customer.GetShippingLabel();
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Creating addresses
+        Address address1 = new Address("23 Mpumula St", "City of Kings", "CK", "Zimbabwe");
+        Address address2 = new Address("456 Msasa Ave", "Kwekwe City ", "KC", "Zimbbabwe");
+
+        // Creating customers
+        Customer customer1 = new Customer("Nyasha gumbogurru", address1);
+        Customer customer2 = new Customer("Tatenda Kazai", address2);
+
+        // Creating orders
+        Order order1 = new Order(customer1);
+        Order order2 = new Order(customer2);
+
+        // Adding products
+        order1.AddProduct(new Product("Laptop", "E4310", 1200, 1));
+        order1.AddProduct(new Product("Mouse", "M456", 25, 2));
+
+        order2.AddProduct(new Product("Keyboard", "K789", 45, 1));
+        order2.AddProduct(new Product("Monitor", "M999", 200, 2));
+
+        // Displaying order details
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.GetTotalPrice()}");
+
+        Console.WriteLine();
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.GetTotalPrice()}");
     }
 }
